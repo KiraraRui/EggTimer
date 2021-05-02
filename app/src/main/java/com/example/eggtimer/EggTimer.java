611e0c6b1b8611ee922628b8ed82e563fb1579eb
Kira0698 <kira0698@zbc.dk>
2021-05-02 22:50:19 +0200

    Update on a bit of everything

--- app/src/main/java/com/example/eggtimer/EggTimer.java
+++ app/src/main/java/com/example/eggtimer/EggTimer.java
@@ -1,4 +1,63 @@
 package com.example.eggtimer;
+import android.os.CountDownTimer;
+import java.util.ArrayList;
 
-public class EggTimer {
+public class EggTimer extends Thread {
+
+
+    private CountDownTimer timer;
+
+    private long remainingBoilingTime;
+
+    private boolean isBoiling;
+
+    private ArrayList<EggTimerListener> listeners;
+
+    public EggTimer(long timeToCook) {
+        this.remainingBoilingTime = timeToCook;
+        listeners = new ArrayList<>();
+    }
+
+    @Override
+    public void run() {
+        if (!isBoiling) {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        timer = new CountDownTimer(remainingBoilingTime, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                remainingBoilingTime = millisUntilFinished;
+
+                for (EggTimerListener l : listeners) {
+                    l.onCountDown(remainingBoilingTime);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        isBoiling = true;
+    }
+
+    public void stopTimer() {
+        timer.cancel();
+        isBoiling = false;
+    }
+
+
+    public void addListener(EggTimerListener listener) {
+        listeners.add(listener);
+    }
+
+
+    public void removeListener(EggTimerListener listener) {
+        listeners.remove(listener);
+    }
 }
+
